2583b1e1c89a6a2a63d0e4a25f8138c82bdd90a4
kamonohashi22 <nyankodezozaru22@gmail.com>
2024-03-07 13:40:41 +0900

    サイドナビゲーションバー作成

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import SideNav from "./ui/components/sidenavi";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,6 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <SideNav />
       <body className={inter.className}>{children}</body>
     </html>
   );
