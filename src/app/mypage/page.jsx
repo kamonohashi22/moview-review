f7f4343e88aacd02a510a138ae404652695fa30a
sunamoto-mikiya <miki38gtr@gmail.com>
2024-03-08 10:14:39 +0900

    インデント修正

--- src/app/mypage/page.jsx
+++ src/app/mypage/page.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Textarea } from "@/components/ui/textarea"
+import { Textarea } from "@/components/ui/textarea";
 
 import MovieCard from "../ui/components/card";
 import CreateCard from "../ui/components/createcard";
@@ -7,13 +7,11 @@ import CreateCard from "../ui/components/createcard";
 export default function Home() {
   return (
     <main>
-    <div className="bg-slate-300">
-      <h1 className="p-4 text-6xl font-bold text-gray-700">
-          My Page
-      </h1>
-   <MovieCard/>
-   <CreateCard/>
-   </div>
+      <div className="bg-slate-300">
+        <h1 className="p-4 text-6xl font-bold text-gray-700">My Page</h1>
+        <MovieCard />
+        <CreateCard />
+      </div>
     </main>
   );
 }
